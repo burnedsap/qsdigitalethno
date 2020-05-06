cfe723b9cfaeb7d9fc208ba84e8005f2b25c9985
burnedsap <salil458@gmail.com>
2020-05-07 00:46:57 +0530

    new

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@
 //
 
 
-
+//var r = 40;
 
 // Complicated Knob
 // Daniel Shiffman <http://www.shiffman.net>
@@ -18,20 +18,21 @@ let img;
 let back;
 function preload() {
 	img = loadImage('dial.png');
-	back = loadImage('background.png');
+	back = loadImage('background-c.png');
 }
 
 var dragging = false; // Is the slider being dragged?
 var rollover = false; // Is the mouse over the slider?
 
 // Circle variables for knob
-var x = 160;
-var y = 180;
-var r = 40;
+
 
 // Knob angle
 var angle = 0;
 var count = 0;
+var calcAngle;
+var degree;
+var stop;
 
 // Offset angle for turning knob
 var offsetAngle = 0;
@@ -39,11 +40,38 @@ function setup() {
 	var canvas = createCanvas(windowWidth, windowHeight);
 	canvas.parent('sketch-holder');
 	imageMode(CENTER);
+	rectMode(CENTER);
 }
 
 function draw() {
 	background('#C2C1C1');
-	image(back, windowWidth / 2, windowHeight / 2, 1021 / 2, 1201 / 2);
+	
+	//White Backlight
+	push();
+	translate(windowWidth / 2, windowHeight / 2);
+	fill(0);
+	rect(0, 0, 1001 / 2, 1001 / 2);
+	console.log(degree);
+	if ((degree < 264) && (degree > 91)) {
+		rotate(angle);
+		stop = angle;
+	} else {
+		rotate(stop);
+	}
+	
+	stroke(255);
+	//	} else {
+	//		stroke(0);
+	//	}
+		strokeWeight(50);
+	line(0, 0, 180, 0);
+	pop();
+	
+	
+	//Background
+	image(back, windowWidth / 2, windowHeight / 2, 1201 / 2, 1201 / 2);
+	
+	//Rotate Dial
 	if (count === 0) {
 				
 		  // Is it being dragged?
@@ -62,28 +90,29 @@ function draw() {
 		}
 		  // Draw ellipse for knob
 		  push();
-		translate(windowWidth/2, windowHeight/2);
+		translate(windowWidth / 2, windowHeight / 2);
 		rotate(angle);
-		image(img, 0, 0, 411/2, 410/2);
-		//  ellipse(0, 0, r*2, r*2);
-		  line(0, 0, r, 0);
-		pop();
+		image(img, 0, 0, 411 / 2, 410 / 2);
+				//  ellipse(0, 0, r*2, r*2);
+		//		  strokeWeight(1);
+		//		  line(0, 0, 90, 0);
+				  
+				pop();
 		fill(0);
 		
 		
 		  // Map is an amazing function that will map one range to another!
 		  // Here we take the slider's range and map it to a value between 0 and 255
 		  // Our angle is either between
-		  var calcAngle = 0;
 		if (angle < 0) {
 			calcAngle = map(angle,  - PI, 0, PI, 0);
 		} else if (angle > 0) {
 			calcAngle = map(angle, 0, PI, TWO_PI, PI);
 		}
 		
+		degree = int(degrees(calcAngle));
 		textAlign(CENTER);
-		text(int(degrees(calcAngle)), x, y +r + 20);
-		var degree = int(degrees(calcAngle));
+		text(int(degrees(calcAngle)), windowWidth / 2 , windowHeight / 2 + 40 + 20);
 		if (dragging && degree < 10) {
 			count == 2;
 		}
@@ -98,11 +127,11 @@ function draw() {
 
 function mousePressed() {
 	  // Did I click on slider?
-	  if (dist(mouseX, mouseY, windowWidth/2, windowHeight/2) < 410) {
+	  if (dist(mouseX, mouseY, windowWidth / 2, windowHeight / 2) < 410) {
 		dragging = true;
 	    // If so, keep track of relative location of click to corner of rectangle
-	    var dx = mouseX - windowWidth/2;
-		var dy = mouseY - windowHeight/2;
+	    var dx = mouseX - windowWidth / 2;
+		var dy = mouseY - windowHeight / 2;
 		offsetAngle = atan2(dy, dx) - angle;
 	}
 }
